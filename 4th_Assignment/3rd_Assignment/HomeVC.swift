dc06c26feec0ff85c9d34c86ce7668d3e0a62464
yujin <glisteneugene@gmail.com>
2020-11-13 18:16:38 +0900

    화면이 두 번 올라간다...

--- 4th_Assignment/3rd_Assignment/HomeVC.swift
+++ 4th_Assignment/3rd_Assignment/HomeVC.swift
@@ -11,21 +11,25 @@ class HomeVC: UIViewController {
 
     @IBOutlet var partImageView: UIImageView!
     @IBOutlet var partLabel: UILabel!
-    @IBOutlet var partNameLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
     
     var part: String?
+    var name: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     override func viewWillAppear(_ animated: Bool) {
-            setLabel()
+        setLabel()
         }
     func setLabel(){
         if let part = self.part {
-            self.partNameLabel.text = part
-            self.partNameLabel.sizeToFit()
+            self.partLabel.text = part
+            self.partLabel.sizeToFit()
             self.partImageView.image = UIImage(named: part)
+        }
+        if let name = self.name {
+            self.nameLabel.text = "\(name)님 안녕하세요"
         }
     }
     @IBAction func touchUpLogin(_ sender: UIButton) {
--- 4th_Assignment/3rd_Assignment/LoginVC.swift
+++ 4th_Assignment/3rd_Assignment/LoginVC.swift
@@ -18,12 +18,29 @@ class LoginVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        partTextfield.delegate = self
+        nameTextfield.delegate = self
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        registerForKeyboardNotifications()
         }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterForKeyboardNotifications()
+    }
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShowNotification(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHideNotification(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
     
     @IBAction func touchUpHome(_ sender: UIButton) {
         let presentVC = self.presentingViewController as? UITabBarController
@@ -31,8 +48,48 @@ class LoginVC: UIViewController {
         
         guard let homeVC = presentVC?.selectedViewController as? HomeVC else { return }
         homeVC.part = self.partTextfield.text
-        
+        homeVC.name = self.nameTextfield.text
         self.presentingViewController?.dismiss(animated: true)
     }
 
+    @objc func keyboardWillShowNotification(_ notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                if keyboardSize.height == 0.0 || keyboardShown == true {
+                    return
+                }
+            
+                UIView.animate(withDuration: 0.33, animations: { () -> Void in
+                    if self.originY == nil { self.originY = self.rootView.frame.origin.y }
+                    self.rootView.frame.origin.y = self.originY! - keyboardSize.height/3
+                }, completion: { _ in
+                    self.keyboardShown = true
+                })
+        }
+    }
+
+    @objc func keyboardWillHideNotification(_ notification: Notification) {
+        if ((notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+              if keyboardShown == false {
+                  return
+              }
+
+              UIView.animate(withDuration: 0.33, animations: { () -> Void in
+                guard self.originY != nil else { return }
+                self.rootView.frame.origin.y = self.originY!
+              }, completion: { _ in
+                    self.keyboardShown = false
+              })
+            }
+    }
+}
+extension LoginVC: UITextFieldDelegate {
+    
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    if textField == partTextfield {
+      nameTextfield.becomeFirstResponder()
+    } else {
+        nameTextfield.endEditing(true)
+    }
+    return true
+  }
 }
